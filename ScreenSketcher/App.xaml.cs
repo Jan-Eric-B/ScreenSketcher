54b5c5932cde5bfe24be19184abc95699c6a797c
Jan-Eric Bednarek <debednaj@krohne.com>
2025-02-04 16:48:54 +0100

    Added Icon, DLL references & notifyIcon

--- ScreenSketcher/App.xaml.cs
+++ ScreenSketcher/App.xaml.cs
@@ -1,14 +1,59 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
 namespace ScreenSketcher
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
-    }
+        private NotifyIcon notifyIcon = new();
+        private MainWindow mainWindow = new();
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            mainWindow = new();
+
+            notifyIcon = new NotifyIcon()
+            {
+                Icon = new("Icon.ico"),
+                Visible = true,
+                Text = "ScreenSketcher",
+                ContextMenuStrip = new ContextMenuStrip()
+                {
+                    Items =
+                    {
+                        new ToolStripMenuItem("Open", null, (s, ev) => { MainWindow_Open(); }),
+                        new ToolStripMenuItem("Close", null, (s, ev) => { Current.Shutdown(); })
+                    }
+                }
+            };
+
+            notifyIcon.MouseDown +=
+               (sender, args) =>
+               {
+                   if (args.Button == MouseButtons.Left)
+                   {
+                       MainWindow_Open();
+                   }
+               };
+
+            MainWindow_Hide();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            notifyIcon?.Dispose();
+            base.OnExit(e);
+        }
+
+        private void MainWindow_Open()
+        {
+            mainWindow.Visibility = Visibility.Visible;
+        }
+
+        private void MainWindow_Hide()
+        {
+            mainWindow.Visibility = Visibility.Hidden;
+        }
+    }
 }
--- ScreenSketcher/MainWindow.xaml.cs
+++ ScreenSketcher/MainWindow.xaml.cs
@@ -1,21 +1,40 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ScreenSketcher
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        #region DLL Import
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetDesktopWindow();
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetWindowDC(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("gdi32.dll")]
+        private static extern bool BitBlt(
+            IntPtr hdcDest, int xDest, int yDest, int wDest, int hDest,
+            IntPtr hdcSource, int xSrc, int ySrc, int dwRop);
+
+        [DllImport("gdi32.dll")]
+        private static extern bool DeleteObject(IntPtr hObject);
+
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr CreateCompatibleBitmap(IntPtr hdc, int nWidth, int nHeight);
+
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr CreateCompatibleDC(IntPtr hdc);
+
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr SelectObject(IntPtr hdc, IntPtr hgdiobj);
+
+        #endregion DLL Import
+
         public MainWindow()
         {
             InitializeComponent();
